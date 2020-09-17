4cdf781667e544ba91f65345b408d94cb2d7f190
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-17 03:23:18 +0000

    adding findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  const fail = "🔴";
+  const pass = "✅";
+  if (actual !== expected) {
+    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(inputObject, value) {
+  for (let element in inputObject) {
+    if (inputObject[element] === value) {
+      return element;
+    }
+  }
+};
+
+
+
+// test cases
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
