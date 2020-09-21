57b3ad1219dddff0f64b30461754b49e0f8c23d7
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-21 20:54:29 +0000

    adding module export and refractor the code

--- tail.js
+++ tail.js
@@ -2,25 +2,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const assertEqual = function(actual, expected) {
-  const fail = "🔴";
-  const pass = "✅";
-  if (actual !== expected) {
-    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// test case 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-// Test Case: Check the original array
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
