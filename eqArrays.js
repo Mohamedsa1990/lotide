42f59eab42a7da451a1412b334fc52b40ba00681
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-21 21:05:48 +0000

    adding module export and refractor

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const fail = "🔴";
-  const pass = "✅";
-  if (actual !== expected) {
-    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const eqArrays = function(array1,array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -20,14 +10,4 @@ const eqArrays = function(array1,array2) {
   return true;
 };
 
-
-// test case
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-// test case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
