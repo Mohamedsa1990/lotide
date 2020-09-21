7aa3cdffd7b0c4dec829771564cbbf6cbcdf64e5
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-21 20:34:19 +0000

    adding module export and moving the test code

--- assertEqual.js
+++ assertEqual.js
@@ -9,8 +9,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 55);
+module.exports = assertEqual;
